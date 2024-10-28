58e97a49b3cee59fffbeaffdfbc123dafe21ef7e
maxwellfundi <fundimaxwell@gmail.com>
2024-10-28 13:34:59 +0200

    addining intial files

---
+++ project_practice/project1_individual.R
@@ -0,0 +1,41 @@
+#install and load ggplot2
+
+
+#load mpg data 
+
+
+#view the data
+
+
+#Visualize the distribution of car classes (class variable) with a bar plot and determine the
+#proportion of each car type. Identify the most common car type.
+
+
+
+
+
+#Engine Size and Fuel Efficiency
+ #Plot a scatterplot of engine displacement (displ) vs. highway fuel efficiency (hwy).
+ #Describe any observable trend. Does a larger engine size generally correlate with lower fuel efficiency?
+  
+
+
+#Analyzing Drive Type
+#Create subsets for each drive type (drv), i.e., f (front-wheel), r (rear-wheel), and 4 (4wd).
+# Compare the average highway fuel efficiency (hwy) across these subsets using mean() and sd().
+# Identify which drive type tends to have the highest and lowest fuel efficiency.
+
+
+
+#Exploring Fuel Economy by Cylinder Count
+# Visualize the distribution of city fuel economy (cty) across cars with different numbers of cylinders (cyl) using a boxplot.
+# Determine if there’s a noticeable trend in fuel efficiency with increasing cylinder count.
+
+
+
+
+#Factors Influencing Fuel Efficiency
+#Investigate which variables might influence fuel efficiency (cty and hwy) the most.
+# Use appropriate visualizations (scatterplots, boxplots) to examine relationships between class, displ, cyl, and fuel efficiency.
+# Summarize your findings and suggest possible reasons for the relationships observed.
+
