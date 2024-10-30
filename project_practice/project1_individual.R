af7199402763efeb39017ec1e6dfe9974e80f8b9
maxwellfundi <fundimaxwell@gmail.com>
2024-10-30 16:43:01 +0200

    Adding initial plots

--- project_practice/project1_individual.R
+++ project_practice/project1_individual.R
@@ -1,23 +1,36 @@
 #install and load ggplot2
-
+install.packages("ggplot2")
+library(ggplot2)
 
 #load mpg data 
+data(mpg)
 
+#view first few rows of the data 
+head(mpg)
 
-#view the data
+#view the dataframe
+View(mpg)
 
 
 #Visualize the distribution of car classes (class variable) with a bar plot and determine the
 #proportion of each car type. Identify the most common car type.
 
+bar <- ggplot(data = mpg, mapping = aes(x=class)) + geom_bar() 
 
+#view barchart
+bar
 
-
+#adding percentages to the barchart
+barwithperct <- ggplot(data= mpg, mapping = aes(x=class)) + geom_bar() + geom_text(stat = 'count', aes(label = paste0(round(..count../sum(..count..)*100, 2), "%")))
+barwithperct
 
 #Engine Size and Fuel Efficiency
  #Plot a scatterplot of engine displacement (displ) vs. highway fuel efficiency (hwy).
  #Describe any observable trend. Does a larger engine size generally correlate with lower fuel efficiency?
-  
+
+scatter <- ggplot(data = mpg, mapping = aes(x=displ, y=hwy)) + geom_point()
+scatter
+
 
 
 #Analyzing Drive Type
@@ -30,7 +43,8 @@
 #Exploring Fuel Economy by Cylinder Count
 # Visualize the distribution of city fuel economy (cty) across cars with different numbers of cylinders (cyl) using a boxplot.
 # Determine if there’s a noticeable trend in fuel efficiency with increasing cylinder count.
-
+bplot <- ggplot(data = mpg, mapping = aes(x=cty, y= as.factor(cyl))) + geom_boxplot()
+bplot
 
 
 
