528623adc343779ddef6360791787d54a6b6135c
maxwellfundi <fundimaxwell@gmail.com>
2024-10-30 17:54:09 +0200

    adding subsets -4wd, front and rear

--- project_practice/project1_individual.R
+++ project_practice/project1_individual.R
@@ -2,8 +2,15 @@
 install.packages("ggplot2")
 library(ggplot2)
 
+#install and load tidyverse package
+install.packages("tidyverse")
+library(tidyverse)
+
 #load mpg data 
 data(mpg)
+
+#geting info about the dataframe 
+str(mpg)
 
 #view first few rows of the data 
 head(mpg)
@@ -21,7 +28,7 @@ bar <- ggplot(data = mpg, mapping = aes(x=class)) + geom_bar()
 bar
 
 #adding percentages to the barchart
-barwithperct <- ggplot(data= mpg, mapping = aes(x=class)) + geom_bar() + geom_text(stat = 'count', aes(label = paste0(round(..count../sum(..count..)*100, 2), "%")))
+barwithperct <- ggplot(data= mpg, mapping = aes(x=class)) + geom_bar() + geom_text(stat = 'count', aes(label = paste0(round(..count../sum(..count..)*100, 2), "%"))) 
 barwithperct
 
 #Engine Size and Fuel Efficiency
@@ -37,13 +44,30 @@ scatter
 #Create subsets for each drive type (drv), i.e., f (front-wheel), r (rear-wheel), and 4 (4wd).
 # Compare the average highway fuel efficiency (hwy) across these subsets using mean() and sd().
 # Identify which drive type tends to have the highest and lowest fuel efficiency.
+front_wheel <- mpg %>% filter(drv == "f") %>% select(hwy,drv) 
+View(front_wheel)
+summary(front_wheel, )
+mean(front_wheel$hwy)
+sd(front_wheel$hwy)
 
+#rear wheel 
+rear_wheel <- mpg %>% filter(drv == "r") %>% select(hwy,drv)
+View(rear_wheel)
+summary(rear_wheel)
+mean(rear_wheel$hwy)
+sd(rear_wheel$hwy)
 
+#4 wd
+fwd <- mpg %>% filter(drv == "4") %>% select(hwy,drv)
+view(fwd)
+summary(fwd)
+mean(fwd$hwy)
+sd(fwd$hwy)
 
 #Exploring Fuel Economy by Cylinder Count
 # Visualize the distribution of city fuel economy (cty) across cars with different numbers of cylinders (cyl) using a boxplot.
 # Determine if there’s a noticeable trend in fuel efficiency with increasing cylinder count.
-bplot <- ggplot(data = mpg, mapping = aes(x=cty, y= as.factor(cyl))) + geom_boxplot()
+bplot <- ggplot(data = mpg, mapping = aes(x=cty, y= as.factor(cyl))) + geom_boxplot() + coord_flip()
 bplot
 
 
