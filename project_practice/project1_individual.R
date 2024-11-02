cb75a55664e77d70853f0d6e3f86973323481bb7
maxwellfundi <fundimaxwell@gmail.com>
2024-11-02 13:01:11 +0200

    Final work

--- project_practice/project1_individual.R
+++ project_practice/project1_individual.R
@@ -1,15 +1,13 @@
-#install and load ggplot2
+#install and load ggplot2, Tidyverse
 install.packages("ggplot2")
-library(ggplot2)
-
-#install and load tidyverse package
 install.packages("tidyverse")
+library(ggplot2)
 library(tidyverse)
 
 #load mpg data 
 data(mpg)
 
-#geting info about the dataframe 
+#getting info about the dataframe 
 str(mpg)
 
 #view first few rows of the data 
@@ -19,60 +17,70 @@ head(mpg)
 View(mpg)
 
 
-#Visualize the distribution of car classes (class variable) with a bar plot and determine the
-#proportion of each car type. Identify the most common car type.
+#barchart of car classes
 
 bar <- ggplot(data = mpg, mapping = aes(x=class)) + geom_bar() 
-
-#view barchart
 bar
 
 #adding percentages to the barchart
-barwithperct <- ggplot(data= mpg, mapping = aes(x=class)) + geom_bar() + geom_text(stat = 'count', aes(label = paste0(round(..count../sum(..count..)*100, 2), "%"))) 
+barwithperct <- bar + geom_text(stat = 'count', aes(label = paste0(round(..count../sum(..count..)*100, 2), "%")), vjust = -0.25) 
 barwithperct
 
-#Engine Size and Fuel Efficiency
- #Plot a scatterplot of engine displacement (displ) vs. highway fuel efficiency (hwy).
- #Describe any observable trend. Does a larger engine size generally correlate with lower fuel efficiency?
+#Adding title to the graph
+finalbar <- barwithperct + ggtitle("Proportions of car classes")
+finalbar
 
-scatter <- ggplot(data = mpg, mapping = aes(x=displ, y=hwy)) + geom_point()
+#scatterplot of engine displacement (displ) vs. highway fuel efficiency (hwy).
+scatter <- ggplot(data = mpg, mapping = aes(x=displ, y=hwy)) + geom_point() + ggtitle("Scatter of displacement vs highway fuel efficient")
 scatter
 
-
-
-#Analyzing Drive Type
-#Create subsets for each drive type (drv), i.e., f (front-wheel), r (rear-wheel), and 4 (4wd).
-# Compare the average highway fuel efficiency (hwy) across these subsets using mean() and sd().
-# Identify which drive type tends to have the highest and lowest fuel efficiency.
+#creating the front wheel subset 
 front_wheel <- mpg %>% filter(drv == "f") %>% select(hwy,drv) 
-View(front_wheel)
-summary(front_wheel, )
-mean(front_wheel$hwy)
-sd(front_wheel$hwy)
 
-#rear wheel 
+#find mean and SD in 2 decimal places
+round(mean(front_wheel$hwy),2)  
+round(sd(front_wheel$hwy),2)
+
+#creating the rear wheel subset 
 rear_wheel <- mpg %>% filter(drv == "r") %>% select(hwy,drv)
-View(rear_wheel)
-summary(rear_wheel)
-mean(rear_wheel$hwy)
-sd(rear_wheel$hwy)
 
-#4 wd
+#find mean and SD in 2 decimal places
+round(mean(rear_wheel$hwy),2)
+round(sd(rear_wheel$hwy),2)
+
+#creating the 4wd wheel subset 
 fwd <- mpg %>% filter(drv == "4") %>% select(hwy,drv)
-view(fwd)
-summary(fwd)
-mean(fwd$hwy)
-sd(fwd$hwy)
 
-#Exploring Fuel Economy by Cylinder Count
-# Visualize the distribution of city fuel economy (cty) across cars with different numbers of cylinders (cyl) using a boxplot.
-# Determine if there’s a noticeable trend in fuel efficiency with increasing cylinder count.
-bplot <- ggplot(data = mpg, mapping = aes(x=cty, y= as.factor(cyl))) + geom_boxplot() + coord_flip()
+#find mean and SD in 2 decimal places
+round(mean(fwd$hwy),2)
+round(sd(fwd$hwy),2)
+
+#boplot of city fuel economy (cty) across of cylinders (cyl)
+bplot <- ggplot(data = mpg, mapping = aes(x= as.factor(cyl), y= cty)) + geom_boxplot() 
 bplot
 
+#Adding title to the graph
+finalbplot <- bplot + ggtitle("City fuel econony across cylinders")
+finalbplot
 
-#Factors Influencing Fuel Efficiency
-#Investigate which variables might influence fuel efficiency (cty and hwy) the most.
-# Use appropriate visualizations (scatterplots, boxplots) to examine relationships between class, displ, cyl, and fuel efficiency.
-# Summarize your findings and suggest possible reasons for the relationships observed.
+#Boxplot of cty vs class vs cyl
+bplotcty <- ggplot(data = mpg, mapping = aes(y =cty, x = class, fill = as.factor(cyl))) + geom_boxplot() + ggtitle("City fuel efficiency for each class and cylinders")
+bplotcty
+
+#Boxplot of hwy vs class vs cyl
+bplothwy <- ggplot(data = mpg, mapping = aes(y =hwy, x = class, fill = as.factor(cyl))) +geom_boxplot() + ggtitle("Highway fuel efficiency for each class and cylinders")
+bplothwy
+
+
+#scatterplot for cty vs disp vs class 
+scattercty <- ggplot(data =mpg, mapping = aes(x = displ, y = cty, colour = class)) + geom_point() + geom_smooth(method = "lm", se = FALSE) + ggtitle("Comparison of city economy, displacemnet across car classes")
+scattercty
+
+#scatterplot for hwy vs disp vs class
+scatterhwy <- ggplot(data =mpg, mapping = aes(x = displ, y = hwy, colour = class)) + geom_point()+ geom_smooth(method = "lm", se = FALSE) + ggtitle("Comparison of highway economy, displacemnet across car classes")
+scatterhwy
+
+
+
+
 
