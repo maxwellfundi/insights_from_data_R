44632916cba52a4b64b4da6daad066432d0c4675
maxwellfundi <fundimaxwell@gmail.com>
2024-12-07 13:58:56 +0200

    small edits

--- project_practice/extra_credit.R
+++ project_practice/extra_credit.R
@@ -30,6 +30,16 @@ model <- lm(quality ~ fixed.acidity + volatile.acidity + citric.acid +
               alcohol + type, data = train_data)
 
 #view the summary of the model
+options(scipen = 999)
+summary(model)
+
+
+#fitting the linear regression model without citric.acid and chlorides
+model <- lm(quality ~ fixed.acidity + volatile.acidity + residual.sugar + free.sulfur.dioxide + 
+              total.sulfur.dioxide + density + pH + sulphates + 
+              alcohol + type, data = train_data)
+
+#view the summary of the model
 summary(model)
 
 #Predict and view predictions on the wine quality on the test data
@@ -38,12 +48,11 @@ head(predictions)
 
 
 # ggplot2 Scatter Plot for Actual vs Predicted Wine Quality
-ggplot(data = test_data, aes(x = quality, y = predictions)) +
+ggplot(data = test_data, mapping =  aes(x = quality, y = predictions)) +
   geom_point(color = "blue", alpha = 0.5) +
   geom_abline(slope = 1, intercept = 0, color = "red", linetype = "dashed", linewidth = 1) +
   labs(title = "Actual vs Predicted Wine Quality",
-       x = "Actual Wine Quality",
-       y = "Predicted Wine Quality") 
+       x = "Actual Wine Quality", y = "Predicted Wine Quality") + coord_cartesian(xlim = c(2.5, 8), ylim = c(3, 8))
 
 # Calculate Mean Squared Error (MSE)
 mse <- mean((predictions - test_data$quality)^2)
@@ -69,7 +78,7 @@ residuals <- predictions - test_data$quality
 residuals <- data.frame(residuals)
 # Plot residuals using ggplot2
 ggplot(residuals, aes(x = residuals)) +
-  geom_histogram(binwidth = 0.2, fill = "lightblue", color = "black", alpha = 0.7) +
+  geom_histogram(binwidth = 0.9, fill = "lightblue", color = "black", alpha = 0.7) +
   labs(title = "Histogram of Residuals", x = "Residuals", y = "Frequency") 
 
 
